ef8ad291796b514a760f00d28db71ef9716b552c
Ariana Casandra Olaru <126718639+arianacasandra@users.noreply.github.com>
2024-05-27 16:09:33 +0300

    Add files via upload

---
+++ src/main/java/isp/StocksPortfolioManagementApplication/exercise1/Main.java
@@ -0,0 +1,9 @@
+package isp.StocksPortfolioManagementApplication.exercise1;
+
+import isp.lab9.exercise1.ui.LoginJFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        new LoginJFrame();
+    }
+}
---
+++ src/test/java/isp/StocksPortfolioManagementApplication/ExampleTest.java
@@ -0,0 +1,5 @@
+package isp.StocksPortfolioManagementApplication;
+
+
+public class ExampleTest {
+}
